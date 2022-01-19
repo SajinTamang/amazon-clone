4606854cb9cb33b52625aa6978f83ddb25546a0c
SajinTamang <sajin.god1.sl@gmail.com>
2022-01-19 19:30:52 +0800

    finished seting up react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        Amazon Clone
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Amazon Clone</h1>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/checkout" element={<h1>Check out</h1>} />
+          <Route path="/login" element={<h1>Login page</h1>} />
+          <Route path="*" element={<h1>error page</h1>} />
+        </Routes>
+        {/* Home Page */}
+        {/* Checkout */}
+        {/* Login */}
+      </div>
+    </Router>
   );
 }
 
